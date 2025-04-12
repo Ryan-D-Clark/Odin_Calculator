d4a8d2e75863cf971d681fa3c018240bd251814a
Ryan-D-Clark <Ryan@clarkonline.me.uk>
2025-04-12 03:12:26 +0100

    RC: Operators

--- external.js
+++ external.js
@@ -1,4 +1,50 @@
-buttonOne = document.getElementById(1)
-buttonOne.addEventListener("click", () =>{
-    console.log("Test")
-})
+let calculatorDisplay = document.getElementById("calculator-display")
+let operators = document.getElementById("operators")
+
+
+let firstValue = 0
+let secondValue = 0
+let total = 0
+let displayValue = ""
+let operator = ""
+
+for(let x = 0; x < 10; x++){
+    let button = document.getElementById(x)
+    button.addEventListener("click",  () => {
+        displayValue += button.getAttribute("id")
+        refreshCalculatorDisplay()
+    })
+}
+
+for(let child of operators.children){
+    child.addEventListener("click", () =>{
+        if(child.id == "="){
+            if(operator == ""){
+                displayValue = "N/A"
+                refreshCalculatorDisplay()
+                calculatorReset()
+            }
+            else{
+                switch(operator){
+                    case "+":
+                        total = firstValue + secondValue
+                        break
+                    case "-":
+                }
+            }
+        }
+    })
+}
+
+function refreshCalculatorDisplay(){
+    calculatorDisplay.innerText = displayValue
+}
+
+function calculatorReset(){
+    firstValue = 0
+    secondValue = 0
+    total = 0
+    displayValue = ""
+    operator = ""
+}
+
