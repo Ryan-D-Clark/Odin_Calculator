717154bf272a3108ebe0074d4d1822aeca47c3f2
Ryan-D-Clark <Ryan@clarkonline.me.uk>
2025-04-17 00:40:17 +0100

    RC: Operators not done

--- external.js
+++ external.js
@@ -4,6 +4,7 @@ let operators = document.getElementById("operators")
 
 let firstValue = 0
 let secondValue = 0
+let currentValue = 0
 let total = 0
 let displayValue = ""
 let operator = ""
@@ -12,6 +13,7 @@ for(let x = 0; x < 10; x++){
     let button = document.getElementById(x)
     button.addEventListener("click",  () => {
         displayValue += button.getAttribute("id")
+        currentValue += button.getAttribute("id")
         refreshCalculatorDisplay()
     })
 }
@@ -25,15 +27,57 @@ for(let child of operators.children){
                 calculatorReset()
             }
             else{
+                secondValue = parseInt(currentValue)
                 switch(operator){
                     case "+":
                         total = firstValue + secondValue
                         break
                     case "-":
+                        total = firstValue - secondValue
+                        break;
+                    case "*":
+                        total = firstValue * secondValue
                         
                 }
             }
+            displayValue = total
+            refreshCalculatorDisplay()
+            console.log("Equal pressed")
+            console.log(total)
         }
+        else{
+            switch(child.id){
+                case "+":
+                    operator = "+"
+                    firstValue = parseInt(currentValue)
+                    displayValue += "+"
+                    currentValue = 0
+                    break
+                case "-":
+                    operator = "-"
+                    firstValue = parseInt(currentValue)
+                    displayValue += "-"
+                    currentValue = 0
+                    break
+                case "x":
+                    operator = "*"
+                    firstValue = parseInt(currentValue)
+                    displayValue += "x"
+                    currentValue = 0
+                    break
+                case "/":
+                    operator = "/"
+                    firstValue = parseInt(currentValue)
+                    displayValue += "/"
+                    currentValue = 0
+                    break
+                case "C":
+                    calculatorReset()
+                    break
+            }
+        }
+        refreshCalculatorDisplay()
+        console.log("Operator", operator)
     })
 }
 
